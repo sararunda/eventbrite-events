a28c0bec7692ae1bbe0041ceb39f2b0e70e08999
Sara Fernández Ramos <sararunda@hotmail.com>
2022-06-15 12:10:24 +0200

    delete component not necesary

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,19 +3,15 @@ const EventList = (props) => {
   const renderList = () => {
     return props.events
 
-      .filter(
-        (event) => {
-          if (props.filter === 'online') {
-            return event.city === 'online';
-          } else if (props.filter === 'physical') {
-            return event.city !== 'online';
-          } else {
-            return true;
-          }
+      .filter((event) => {
+        if (props.filter === 'online') {
+          return event.city === 'online';
+        } else if (props.filter === 'physical') {
+          return event.city !== 'online';
+        } else {
+          return true;
         }
-
-        //props.filter === '' ? true : event.city === props.filter
-      )
+      })
 
       .map((event, index) => {
         return (
